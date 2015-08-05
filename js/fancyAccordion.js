2cfde6aef6edfaae83a7acb5dde27e734a4413f2
maddydeason <mdeason89@gmail.com>
2015-08-05 17:34:37 -0400

    accordion js change

--- js/fancyAccordion.js
+++ js/fancyAccordion.js
@@ -1,8 +1,19 @@
-$(document).ready(function() {
-  $(".accordion").on("click", "li", function () {
-    $("li.active").find(".content").slideUp("normal");
-    if(!$(this).hasClass("active")) {
-      $(this).find(".content").slideToggle("normal");
+$(document).ready(function($) {
+
+  $('.accordion a').click(function(j) {
+    var dropDown = $(this).closest('li').find('.content');
+
+    $(this).closest('.accordion').find('.content').not(dropDown).slideUp('normal');
+
+    if ($(this).hasClass('active')) {
+      $(this).removeClass('active');
+    } else {
+      $(this).closest('.accordion').find('a.active').removeClass('active');
+      $(this).addClass('active');
     }
+
+    dropDown.stop(false, true).slideToggle('normal');
+
+    j.preventDefault();
   });
-});
+})(jQuery);
