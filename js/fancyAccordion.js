0365b64824169fc3bb92db08d8bbf51b4426b1bb
maddydeason <mdeason89@gmail.com>
2015-08-06 09:51:27 -0400

    updates accordion: learn more changes to close, down arrow changes to up

--- js/fancyAccordion.js
+++ js/fancyAccordion.js
@@ -7,9 +7,12 @@ $(document).ready(function($) {
 
     if ($(this).hasClass('active')) {
       $(this).removeClass('active');
+      $(this).find('.learn-more')[0].innerHTML = "learn more &nbsp;&nbsp;<img class='down-arrow' src='img/down.png'>";
+      //document.getElementByClassName("down-arrow").src = "img/up.png";
     } else {
       $(this).closest('.accordion').find('a.active').removeClass('active');
       $(this).addClass('active');
+      $(this).find('.learn-more')[0].innerHTML = "close &nbsp;&nbsp;<img class='up-arrow' src='img/up.png'>";
     }
 
     dropDown.stop(false, true).slideToggle('normal');
