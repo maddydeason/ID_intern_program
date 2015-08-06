a1ce5d71a749016d3777f932926f53693147f7b6
maddydeason <mdeason89@gmail.com>
2015-08-06 10:57:40 -0400

    cleaned up some html/css, also still working on a minor accordion bug

--- js/fancyAccordion.js
+++ js/fancyAccordion.js
@@ -2,13 +2,14 @@ $(document).ready(function($) {
 
   $('.accordion a').click(function(j) {
     var dropDown = $(this).closest('li').find('.content');
+    //var activeLearnmore = $(this).closest('li').find('learn-more');
 
     $(this).closest('.accordion').find('.content').not(dropDown).slideUp('normal');
+    //$(this).closest('.accordion').find('.learn-more').not(activeLearnmore)[0].innerHTML = "learn more &nbsp;&nbsp;<img class='down-arrow' src='img/down.png'>";
 
     if ($(this).hasClass('active')) {
       $(this).removeClass('active');
       $(this).find('.learn-more')[0].innerHTML = "learn more &nbsp;&nbsp;<img class='down-arrow' src='img/down.png'>";
-      //document.getElementByClassName("down-arrow").src = "img/up.png";
     } else {
       $(this).closest('.accordion').find('a.active').removeClass('active');
       $(this).addClass('active');
