0d8ea3bc974591a2abe81ff1b2b5e3f157c4157e
Nan Copeland <nmcopeland1@gmail.com>
2015-08-11 16:01:02 -0400

    updatez
    updating learn more/close functions

--- js/fancyAccordion.js
+++ js/fancyAccordion.js
@@ -1,5 +1,5 @@
 // the accordion now known as sexyAccordian
-$(document).ready(function($) {
+/*$(document).ready(function($) {
 
   $('.accordion a').click(function(j) {
     var dropDown = $(this).closest('.accordion-navigation').find('.content');
@@ -22,4 +22,55 @@ $(document).ready(function($) {
 
     j.preventDefault();
   });
-})(jQuery);
+})(jQuery);*/
+
+$(document).ready(function(){
+  $(".content").hide(); 
+
+  $(".audience-dev").click(function(){
+
+      $(this).toggleClass("active").next().slideToggle("fast");
+      
+      if ($.trim($(this).html()) === '<p><span class="position">audience <br class="show-for-small-only">development</span><span class="learn-more">close &nbsp;&nbsp;<img class="down-arrow" src="img/up.png"></span></p>') {
+          $(this).html('<p><span class="position">audience <br class="show-for-small-only">development</span><span class="learn-more">learn more &nbsp;&nbsp;<img class="down-arrow" src="img/down.png"></span></p>');
+      } 
+      else {
+          $(this).html('<p><span class="position">audience <br class="show-for-small-only">development</span><span class="learn-more">close &nbsp;&nbsp;<img class="down-arrow" src="img/up.png"></span></p>');        
+      }
+      
+      return false; 
+  });
+
+  $(".design").click(function(){
+
+      $(this).toggleClass("active").next().slideToggle("fast");
+      
+      if ($.trim($(this).html()) === '<p><span class="position">design</span><span class="learn-more">close &nbsp;&nbsp;<img class="down-arrow" src="img/up.png"></span></p>') {
+          $(this).html('<p><span class="position">design</span><span class="learn-more">learn more &nbsp;&nbsp;<img class="down-arrow" src="img/down.png"></span></p>');
+      } 
+      else {
+          $(this).html('<p><span class="position">design</span><span class="learn-more">close &nbsp;&nbsp;<img class="down-arrow" src="img/up.png"></span></p>');        
+      }
+      
+      return false; 
+  });
+
+   $("a[href='" + window.location.hash + "']").parent(".intern-department").click();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
